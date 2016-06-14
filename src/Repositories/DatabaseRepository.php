e61422ddfb48ceeee52b8750bcf42122ca88eb5c
Recca Tsai <recca0120.gmail.com>
2016-06-14 13:32:20 +0800

    only save changed data, add testing

--- src/Repositories/DatabaseRepository.php
+++ src/Repositories/DatabaseRepository.php
@@ -3,15 +3,31 @@
 namespace Recca0120\Config\Repositories;
 
 use Illuminate\Contracts\Config\Repository as RepositoryContract;
+use Illuminate\Contracts\Foundation\Application as ApplicationContract;
+use Recca0120\Config\Config;
 
 class DatabaseRepository extends AbstractRepository
 {
     /**
+     * $original.
+     *
+     * @var array
+     */
+    protected $original = [];
+
+    /**
+     * $key.
+     *
+     * @var string
+     */
+    protected $key = 'configs';
+
+    /**
      * $repository.
      *
-     * @var \Illuminate\Contracts\Config\Repository
+     * @var \Recca0120\Config\Config
      */
-    protected $repository;
+    protected $model;
 
     /**
      * $needUpdate.
@@ -26,13 +42,29 @@ class DatabaseRepository extends AbstractRepository
      * @method __construct
      *
      * @param \Illuminate\Contracts\Config\Repository $config
-     * @param ConfigRepository                        $repository
+     * @param \Recca0120\Config\Config
+     * @param \Illuminate\Contracts\Foundation\Application
      */
-    public function __construct(RepositoryContract $config, ConfigRepository $repository)
+    public function __construct(RepositoryContract $config, Config $model, ApplicationContract $app)
     {
-        parent::__construct($config);
-        $this->repository = $repository;
-        $this->config->set($this->repository->all());
+        parent::__construct($config, $app);
+        $this->original = $config->all();
+        $this->model = $model;
+
+        $data = value(function () {
+            $file = $this->getStorageFile();
+            if (file_exists($file) === true) {
+                return json_decode(file_get_contents($file), true);
+            }
+            $data = $this->getModel()->value;
+            $this->store($data);
+
+            return $data;
+        });
+
+        foreach (array_dot($data) as $key => $value) {
+            $config->set($key, $value);
+        }
     }
 
     /**
@@ -45,10 +77,21 @@ class DatabaseRepository extends AbstractRepository
      */
     public function set($key, $value = null)
     {
-        $this->config->set($key, $value);
-        if ($this->needUpdate === true) {
-            $this->repository->store($this->config->all());
-        }
+        parent::set($key, $value);
+        $this->storeDiff();
+    }
+
+    /**
+     * Unset a configuration option.
+     *
+     * @param string $key
+     *
+     * @return void
+     */
+    public function offsetUnset($key)
+    {
+        $this->set($key, null);
+        $this->storeDiff();
     }
 
     /**
@@ -63,5 +106,92 @@ class DatabaseRepository extends AbstractRepository
     public function needUpdate($status)
     {
         $this->needUpdate = $status;
+    }
+
+    /**
+     * cloneModel.
+     *
+     * @method cloneModel
+     *
+     * @return \Recca0120\Config\Config
+     */
+    protected function cloneModel()
+    {
+        return clone $this->model;
+    }
+
+    /**
+     * getModel.
+     *
+     * @method getModel
+     *
+     * @return \Recca0120\Config\Config
+     */
+    protected function getModel()
+    {
+        return $this->cloneModel()->firstOrCreate([
+            'key' => $this->key,
+        ]);
+    }
+
+    /**
+     * store.
+     *
+     * @method store
+     *
+     * @param mix $data
+     */
+    protected function store($data)
+    {
+        file_put_contents($this->getStorageFile(), json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    }
+
+    /**
+     * storeDiff description.
+     *
+     * @method storeDiff
+     */
+    protected function storeDiff()
+    {
+        if ($this->needUpdate === false) {
+            return;
+        }
+        $diff = $this->arrayDiffAssocRecursive($this->all(), $this->original);
+        if (empty($diff) === false) {
+            $model = $this->getModel();
+            $model->fill(['value' => $diff])->save();
+            $this->store($diff);
+        }
+    }
+
+    /**
+     * arrayDiffAssocRecursive.
+     *
+     * @method arrayDiffAssocRecursive
+     *
+     * @param array $array1
+     * @param array $array2
+     *
+     * @return array
+     */
+    protected function arrayDiffAssocRecursive($array1, $array2)
+    {
+        $difference = [];
+        foreach ($array1 as $key => $value) {
+            if (is_array($value)) {
+                if (!isset($array2[$key]) || !is_array($array2[$key])) {
+                    $difference[$key] = $value;
+                } else {
+                    $new_diff = $this->arrayDiffAssocRecursive($value, $array2[$key]);
+                    if (!empty($new_diff)) {
+                        $difference[$key] = $new_diff;
+                    }
+                }
+            } elseif (!array_key_exists($key, $array2) || $array2[$key] !== $value) {
+                $difference[$key] = $value;
+            }
+        }
+
+        return $difference;
     }
 }
