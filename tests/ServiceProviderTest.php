9eb53d214afca8a523569103ac9833cf5c16f997
Recca Tsai <recca0120.gmail.com>
2016-10-31 21:26:30 +0800

    update

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -28,7 +28,8 @@ class ServiceProviderTest extends PHPUnit_Framework_TestCase
         |------------------------------------------------------------
         */
 
-        $kernel->shouldReceive('pushMiddleware')->with('Recca0120\Config\Middleware\SetConfigRepository')->once();
+        $kernel
+            ->shouldReceive('pushMiddleware')->with('Recca0120\Config\Middleware\SetConfigRepository')->once();
 
         $app
             ->shouldReceive('singleton')->with('Recca0120\Config\Contracts\Repository', m::type('Closure'))->once()->andReturnUsing(function ($className, $closure) use ($app) {
@@ -36,45 +37,6 @@ class ServiceProviderTest extends PHPUnit_Framework_TestCase
             })
             ->shouldReceive('make')->with('Recca0120\Config\Repositories\DatabaseRepository', m::any())->once()
             ->shouldReceive('databasePath')->once()
-            ->shouldReceive('storagePath')->once()->andReturn(__DIR__)
-            ->shouldReceive('runningInConsole')->once()->andReturn(false);
-
-        /*
-        |------------------------------------------------------------
-        | Assertion
-        |------------------------------------------------------------
-        */
-
-        $serviceProvider = new ServiceProvider($app);
-        $serviceProvider->register();
-        $serviceProvider->provides();
-        $serviceProvider->boot($kernel);
-    }
-
-    public function test_running_in_console()
-    {
-        /*
-        |------------------------------------------------------------
-        | Set
-        |------------------------------------------------------------
-        */
-
-        $app = m::mock('Illuminate\Contracts\Foundation\Application');
-        $config = m::mock('Recca0120\Config\Repositories\DatabaseRepository');
-        $kernel = m::mock('Illuminate\Contracts\Http\Kernel');
-
-        /*
-        |------------------------------------------------------------
-        | Expectation
-        |------------------------------------------------------------
-        */
-
-        $app
-            ->shouldReceive('singleton')->with('Recca0120\Config\Contracts\Repository', m::type('Closure'))->once()->andReturnUsing(function ($className, $closure) use ($app) {
-                $closure($app);
-            })
-            ->shouldReceive('make')->with('Recca0120\Config\Repositories\DatabaseRepository', m::any())->once()
-            ->shouldReceive('databasePath')->once()->andReturn(__DIR__)
             ->shouldReceive('storagePath')->once()->andReturn(__DIR__)
             ->shouldReceive('runningInConsole')->once()->andReturn(true);
 
