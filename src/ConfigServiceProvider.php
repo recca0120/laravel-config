a3fc7a51c54b77e30d28e11f02bae181b0052474
recca0120 <recca0120@gmail.com>
2017-03-29 11:38:04 +0800

    protected app

--- src/ConfigServiceProvider.php
+++ src/ConfigServiceProvider.php
@@ -30,7 +30,7 @@ class ConfigServiceProvider extends ServiceProvider
         $this->app->singleton(Repository::class, function ($app) {
             $config = [
                 'protected' => [
-                    'app.locale',
+                    'app',
                     'auth.defaults.guard',
                 ],
                 'path' => $app->storagePath().'/app/',
